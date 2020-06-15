a8f0436762af98ecc2b42e693334e97738daafc0
Adnan Ali <adnan.ahmed.9620@gmail.com>
2020-06-15 23:48:45 +0500

    till Chap 09

---
+++ app.js
@@ -0,0 +1,202 @@
+// Chapter 1
+//Task 1
+// alert('Welcome')
+
+// Task 2
+// alert('Error! Please enter a valid password')
+
+//Task 3
+// alert("JavaScript Alert \n \nWelcome to JS Land... \nHappy Coding!")
+
+//Task 4 (remaing)
+// alert("JavaScript Alert \n \nWelcome to JS Land...")
+// alert("JavaScript Alert \n \nHappy Coding!")
+
+//Task 5 
+// alert("JavaScript Alert\n \nHello... I can run JS through my web brower's console")
+
+
+// Chapter 2
+//Task 1
+// var username;
+
+//Task 2
+// var username = "Adnan Ali"
+
+//Task 3
+// var message = "Hello World";
+// alert(message)
+
+//Task 4
+// var studentname = "Adnan ali";
+// var studentage = "23 year old";
+// var certificatename = "Certified Mobile Application Developer";
+
+
+// alert(studentname);
+// alert(studentage);
+// alert(certificatename);
+
+//Task 5
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza)
+
+//Task 6
+// var email = "adnan.ahmed.9620@gmail.com";
+// alert("My email address is "+email )
+
+//Task 7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book "+ book)
+
+//Task 8 
+// document.write('Yah! I can write HTML content through JavaScript')
+
+//Task 9
+// var code = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(code);
+// document.write(code);
+
+// Chapter 3
+//Task 1
+// var age = 23;
+// alert("I am "+age+" years old")
+
+// Task 2
+// var visitor = 1;
+
+
+//Task 3
+// var birthYear = parseInt(prompt("Enter your birth year"));
+// document.write("My birth year is "+birthYear+"<br> Data type of my declare variable is "+ typeof birthYear)
+// document.write("Data type of my declare variable is "+ typeof birthYear)
+
+//Task 4
+// var visitorName = prompt("Enter your name") , 
+// productTitle = prompt("Enter your product"), 
+// quanTity = parseInt(prompt("Enter your quantity"));
+// document.write("<b>"+visitorName+"</b> ordered <b>"+quanTity+"</b>" +"<b>"+productTitle+"</b> on XYZ Clothing store")
+
+// Chapter 4
+//Task 1
+// var visitorName = prompt("Enter your name") , 
+// productTitle = prompt("Enter your product"), 
+// quanTity = parseInt(prompt("Enter your quantity"));
+
+
+// Task 2
+// Legal
+// var visitorName,
+// visitorname,
+// visitor_name,
+// $visitorname,
+// visitor123;
+
+// // illegal 
+// var 1vistor,
+// @vistor,
+// Avisitor,visitor-name,
+// ~vistor;
+
+
+
+// Task 3
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write('Variable names can only contain,numbers,$ and _.For example: $my_1stVariable')
+// document.write('Variables must begin with a , $ or _,For example $name, _name or name')
+// document.write("Variable names are case sensitive")
+// document.write("Variable names should not be JS keywords")
+
+
+//Chapter 5
+//Task 1
+// var num1 = parseInt(prompt('Enter first number')),
+// num2 = parseInt(prompt('Enter second number'));
+// var sum = num2 + num2;
+
+// document.write("Sume of "+num1+" and "+num2+" is "+ sum)
+
+// Task 2
+
+// var num1 = parseInt(prompt('Enter first number')),
+// num2 = parseInt(prompt('Enter second number'));
+// var sub = num1 - num2;
+// var mul = num1 * num2;
+// var div = num1 / num2;
+// var mod = num1 % num2;
+
+
+// // document.write("Sume of "+num1+" and "+num2+" is "+ sub)
+
+
+// Task 3
+
+
+// Task 4
+// var ticket = 600,
+//  totalTicket = 5, totalPaisa = ticket * totalTicket;
+// document.write("Total cost to buy "+totalTicket+" tickets to a movie is "+totalPaisa+"paisa")
+
+
+//Task 5
+// document.write("Tables of 4<br>");
+// for(var i =1;i<=10;i++){
+//     document.write("4 x "+i+"="+4*i+"<br>")
+// }
+
+
+// Task 6
+// var celciusTemperature = parseInt(prompt("enter celcius value to convert"));
+// var farenhiettemperature = parseInt(prompt("enter farenheit value to convert"))
+
+// var cCalculate = (farenhiettemperature -32) * 5/9;
+// var fCalculate = (celciusTemperature * 9/5)+32;
+
+// document.write(celciusTemperature+" 'C is "+ fCalculate+" 'F<br>")
+// document.write(farenhiettemperature+" 'F is "+ cCalculate+" 'C<br>" )
+
+
+// Chapter 6 to 9
+//Task 1
+// var a = 10;
+
+// document.write(`The value of a is ${a}<br>`);
+// document.write(`......................<br>`);
+
+// document.write(`The value of ++a is ${++a}<br>`);
+// document.write(`Now the value of a is: ${a}<br>`);
+
+// document.write(`The value of a++ is ${a++}<br>`);
+// document.write(`Now the value of a is: ${a}<br>`);
+
+// document.write(`The value of --a is ${--a}<br>`);
+// document.write(`Now the value of a is: ${a}<br>`);
+
+// document.write(`The value of a-- is ${a--}<br>`);
+// document.write(`Now the value of a is: ${a}<br>`);
+
+// Task 2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+
+// document.write(`a is ${a}<br>`);
+// document.write(`b is ${b}<br>`);
+// document.write(`result is ${result}<br>`)
+
+//Task 3
+// var greet = prompt("Enter your name");
+
+// document.write(`Welcome ${greet}`)
+
+// Task 5
+// var num = parseInt(prompt('Enter number',5));
+
+// document.write("Tables of "+num+"<br>");
+// for(var i =1;i<=10;i++){
+//     document.write(num+" x "+i+"="+num*i+"<br>")
+// }
+
+
+
+
+
